064bce3bcf2c299cbffdcf5ba668c8beed20a5c6
Nimish <np4447292@gmail.com>
2026-06-15 10:59:24 +0530

    registen , login, JWT token generation successfully implement

---
+++ controllers/user.controller.js
@@ -0,0 +1,116 @@
+const User = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const register = async(req,res)=>{
+    try{
+        const {fullname,email,phoneNumber,password,role} = req.body;
+
+        if(!fullname || !email || !phoneNumber || !password || !role){
+            return res.status(400).json({
+                message:"Something is missing",
+                success:false
+            });
+        }
+
+        const user = await User.findOne({email});
+
+        if(user){
+            return res.status(400).json({
+                message:"User already exists",
+                success:false
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password,10);
+
+        await User.create({
+            fullname,
+            email,
+            phoneNumber,
+            password:hashedPassword,
+            role
+        });
+
+        return res.status(201).json({
+            message:"Account created successfully",
+            success:true
+        });
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            message:"Internal Server Error",
+            success:false
+        });
+    }
+}
+
+const login = async(req,res)=>{
+    try{
+        const {email,password} = req.body;
+
+        if(!email || !password){
+            return res.status(400).json({
+                message:"Something is missing",
+                success:false
+            });
+        }
+
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(400).json({
+                message:"Incorrect email or password",
+                success:false
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password);
+
+        if(!isMatch){
+            return res.status(400).json({
+                message:"Incorrect password",
+                success:false
+            });
+        }
+
+        const token = jwt.sign(
+            {userId:user._id},
+            process.env.JWT_SECRET,
+            {
+                expiresIn:process.env.JWT_EXPIRE
+            }
+        );
+
+        const safeUser = {
+            _id:user._id,
+            fullname:user.fullname,
+            email:user.email,
+            role:user.role,
+            phoneNumber:user.phoneNumber
+        };
+
+        return res.cookie("token",token,{
+            maxAge:7*24*60*60*1000,
+            httpOnly:true,
+            sameSite:"strict"
+        })
+        .status(200)
+        .json({
+            message:`Welcome back ${user.fullname}`,
+            success:true,
+            user:safeUser
+        });
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            message:"Internal Server Error",
+            success:false
+        });
+    }
+}
+
+module.exports = {
+    register,
+    login
+};
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express=require('express');
 const dotenv=require('dotenv');
 const cors = require("cors");
 const connectDB=require('./config/db');
+const cookieParser = require("cookie-parser");
+const userRoute = require("./routes/user.route");
 dotenv.config();
 
 connectDB();
@@ -15,12 +17,15 @@ app.use(
     })
 );
 
+app.use(cookieParser());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/',(req,res)=>{
     res.send("HireHub running")
 });
+app.use("/api/v1/user", userRoute);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on ${process.env.PORT}`);
